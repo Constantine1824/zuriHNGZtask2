35b850a8bf17bdc54e6b1080a9f6e0f68b1baac1
Ayomide <ayomidet905@gmail.com>
2023-09-13 14:06:44 +0100

    Changed directory

--- task2/Core/urls.py
+++ task2/Core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from .views import CRUDApiView
 
 urlpatterns = [
-    re_path(r'^/(?P<id>\d+)?(?P<name>\w+)?$', CRUDApiView.as_view(), name='main'),
+    re_path(r'(/)?(?P<id>\d+)?(?P<name>\w+)?$', CRUDApiView.as_view(), name='main'),
 ]
